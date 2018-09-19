b917657d37d6788d934eafd3b5d6481887e3bbeb
Vijaya Venu <sandeshjkota@gmail.com>
2018-09-19 14:13:22 +0530

    Add files via upload

---
+++ push-notifications/worker.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+// listen to push event and show notification
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Push Message';
+    const options = {
+        body: event.data.text(),
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    console.log(event.data);
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+});
